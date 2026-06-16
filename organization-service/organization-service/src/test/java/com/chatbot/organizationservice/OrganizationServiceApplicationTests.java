c8603504dff624eb41868653dc221784bfa72a21
Maheshwari <ujjawal.maheshwari@accenture.com>
2026-06-16 11:29:09 +0530

    application.yml

--- organization-service/organization-service/src/test/java/com/chatbot/organizationservice/OrganizationServiceApplicationTests.java
+++ organization-service/organization-service/src/test/java/com/chatbot/organizationservice/OrganizationServiceApplicationTests.java
@@ -1,13 +1,29 @@
+// package com.chatbot.organizationservice;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class OrganizationServiceApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
+
 package com.chatbot.organizationservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class OrganizationServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
